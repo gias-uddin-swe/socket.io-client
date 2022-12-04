37fbd173795d4893d0a9457ac7fbe08c49e7d0cb
gias-uddin-swe <nirobh753@gmail.com>
2022-12-04 18:51:50 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import io from "socket.io-client";
+import { useEffect, useState } from "react";
+const socket = io.connect("http://localhost:5000/");
 
 function App() {
+  const [message, setMessage] = useState("");
+  const [getMessage, setGetMessage] = useState("");
+  const [room, setRoom] = useState("");
+
+  const handleSend = () => {
+    socket.emit("reactEvent", { message, room });
+  };
+
+  useEffect(() => {
+    socket.on("showMessage", (data) => {
+      console.log(data);
+      setGetMessage(data.message);
+    });
+  }, [socket]);
+
+  const hanldeRoom = () => {
+    socket.emit("joinRoom", room);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <h1 className="h1">sender:{message}</h1>
+        <h1 className="h1">Receiver: {getMessage}</h1>
+      </div>
+
+      <input
+        onBlur={(e) => setRoom(e.target.value)}
+        type="text"
+        placeholder="ROom...."
+      />
+      <button onClick={hanldeRoom}>Join room</button>
+      <br />
+      <input
+        onBlur={(e) => setMessage(e.target.value)}
+        type="text"
+        placeholder="message...."
+      />
+      <br />
+      <button onClick={handleSend}>send</button>
     </div>
   );
 }
